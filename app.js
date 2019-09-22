860fba7627255bf7a4250f48cc99ef3082dbc31d
Angelo Palmieri <palmieri.ang@gmail.com>
2019-09-22 16:31:05 +0100

    ejs template

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', {
     list: ['a', 'b'],
+    title: 'Library',
   });
 });
 
