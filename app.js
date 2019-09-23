408968522c8c04f100499027c4f8597b888a1ebb
Angelo Palmieri <palmieri.ang@gmail.com>
2019-09-23 21:26:13 +0100

    router function

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,106 +15,18 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const books = [
-  {
-    title: 'The Lightning Thief',
-    genre: '',
-    author: 'Rick Riordan',
-    read: false
-  },
-  {
-    title: 'A Series of Unfortunate Events',
-    genre: '',
-    author: 'Lemony Snicket',
-    read: false
-  },
-  {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  },
-  {
-    title: 'Les Misérables',
-    genre: 'Historical Fiction',
-    author: 'Victor Hugo',
-    read: false
-  },
-  {
-    title: 'The Time Machine',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false
-  },
-  {
-    title: 'A Journey into the Center of the Earth',
-    genre: 'Science Fiction',
-    author: 'Jules Verne',
-    read: false
-  },
-  {
-    title: 'The Dark World',
-    genre: 'Fantasy',
-    author: 'Henry Kuttner',
-    read: false
-  },
-  {
-    title: 'The Wind in the Willows',
-    genre: 'Fantasy',
-    author: 'Kenneth Grahame',
-    read: false
-  },
-  {
-    title: 'Life On The Mississippi',
-    genre: 'History',
-    author: 'Mark Twain',
-    read: false
-  },
-  {
-    title: 'Childhood',
-    genre: 'Biography',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  }
+const nav = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Authors' }
 ];
 
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render('books', {
-      nav: [
-        {
-          link: '/books',
-          title: 'Books'
-        },
-        {
-          link: '/authors',
-          title: 'Authors'
-        }
-      ],
-      title: 'Library',
-      books
-    });
-  });
-
-bookRouter.route('/single')
-  .get((req, res) => {
-    res.send('Hello single books');
-  });
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
   res.render('index', {
-    nav: [
-      {
-        link: '/books',
-        title: 'Books'
-      },
-      {
-        link: '/authors',
-        title: 'Authors'
-      }
-    ],
+    nav,
     title: 'Library'
   });
 });
