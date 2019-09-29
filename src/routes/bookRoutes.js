458188988101c5767454b6359b37c722b8b3d5c8
Angelo Palmieri <palmieri.ang@gmail.com>
2019-09-29 11:17:29 +0100

    mongodb - setup - bookRoutes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
 
@@ -68,11 +70,30 @@ function router(nav) {
 
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('booksListView', {
-        nav,
-        title: 'Library',
-        books
-      });
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+          const books = await col.find().toArray();
+
+          res.render('booksListView', {
+            nav,
+            title: 'Library',
+            books
+          });
+        } catch (err) {
+          debug(err.stack);
+        }
+        client.close();
+      }());
     });
 
   bookRouter.route('/:id')
